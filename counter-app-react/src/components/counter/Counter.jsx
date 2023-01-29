05e6efcda46f4e3e65db5fff799946b2ece751d6
Filip Grabowski <grabfilip2007@gmail.com>
2023-01-29 14:32:06 +0100

    [/] Basic logic

---
+++ counter-app-react/src/components/counter/Counter.jsx
@@ -0,0 +1,15 @@
+export default function Counter() {
+
+    function addCounterFunction() {
+        console.log('Add button clicked')
+    }
+
+    return (
+        <div className="Counter">
+            <span className="count">0</span>
+            <div>
+                <button className="counterButton" onClick={addCounterFunction}>+1</button>
+            </div>
+        </div>
+    )
+}
