7f0f99dbe88252ab48d7ac4287e49acff611fe18
Filip Grabowski <grabfilip2007@gmail.com>
2023-02-13 23:54:11 +0100

    Added logic to buttons

--- counter-app-react/src/components/counter/Counter.jsx
+++ counter-app-react/src/components/counter/Counter.jsx
@@ -1,18 +1,29 @@
+import { useState } from 'react'
 import './Counter.css'
 
 export default function Counter() {
 
-    function addCounterFunction() {
-        console.log('Add button clicked')
+    const [count, setCount] = useState(0);
+
+    function incrementCounterFunction() {
+        setCount(count + 1)
+        console.log(count)
+    }
+
+    function decrementCounterFunction() {
+        setCount(count -1)
     }
 
     return (
         <div className="Counter">
-            <span className="count">0</span>
+            <span className="count">{count}</span>
             <div>
                 <button className="counterButton" 
-                    onClick={addCounterFunction}
+                    onClick={incrementCounterFunction}
                 >+1</button>
+                <button className="counterButton" 
+                    onClick={decrementCounterFunction}
+                >-1</button>
             </div>
         </div>
     )
